31282ab94f058036a184452fb8798f40f584998c
rimonspi12 <rimonmpi@gmail.com>
2021-09-25 15:59:07 +0600

    font awesome packagae install

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
+import './Cart.css';
 const Cart = (props) => {
+    const fontIcon = <FontAwesomeIcon icon={faShoppingCart} />
     const { strCategory } = props.cart;
     console.log(props.cart)
     return (
         <div className="cart-conainer">
-            <h4>Order Details</h4>
+
+            <h4>Order Details {fontIcon}</h4>
             <p>Name: {strCategory}</p>
             <button className="cart-button">show details</button>
         </div>
--- src/component/FoodShow/FoodShow.js
+++ src/component/FoodShow/FoodShow.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart, faDeaf } from '@fortawesome/free-solid-svg-icons';
+import './FoodShow.css';
 import './FoodShow.css';
 const FoodShow = (props) => {
     // console.log(props);
+    const carIcon = <FontAwesomeIcon icon={faShoppingCart} />
     const { idCategory, strCategory, strCategoryThumb, strCategoryDescription } = props.food;
     return (
         <div className="cart">
@@ -9,12 +13,12 @@ const FoodShow = (props) => {
                 <img src={strCategoryThumb} alt="" />
             </div>
             <div className="cart-body">
-                <p>{strCategory}</p>
+                <p className="catogories-style">Categories: {strCategory}</p>
                 <p>{strCategoryDescription.slice(0, 120)}</p>
             </div>
             <div className="btn-style">
                 <div className="first-button">
-                    <button onClick={() => props.handleAddToCart(props.handleAddToCart.strCategory)}>add to cart</button>
+                    <button onClick={() => props.handleAddToCart(props.handleAddToCart.strCategory)}> {carIcon} add to cart</button>
                 </div>
                 <div className="second-button">
                     <button>show details</button>
